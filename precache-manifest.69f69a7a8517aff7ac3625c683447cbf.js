af7e792d2ae348a5fd4246bd37b34d083821071c
unknown <ghodmareabhishek@gmail.com>
2023-03-24 11:55:44 +0530

    Updates

---
+++ precache-manifest.69f69a7a8517aff7ac3625c683447cbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d39a27546d093b732ddb",
+    "url": "/static/css/main.a67950a1.chunk.css"
+  },
+  {
+    "revision": "d39a27546d093b732ddb",
+    "url": "/static/js/main.d39a2754.chunk.js"
+  },
+  {
+    "revision": "3e3c3eb55c09e01a1300",
+    "url": "/static/js/1.3e3c3eb5.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "bd5440dfbcfe3e971529",
+    "url": "/static/js/runtime~main.bd5440df.js"
+  },
+  {
+    "revision": "b23dbcecbde77cbad7342c78745055a9",
+    "url": "/index.html"
+  }
+];
